2ce634578a98c54ecfac66cda0856509c12282b1
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-31 21:07:12 -0300

    feat(User.validation): adding validations to user
    Adding the following validations:
    - update profile picture
    - update description
    - update CV

--- server/src/middleware/User.validator.js
+++ server/src/middleware/User.validator.js
@@ -116,6 +116,96 @@ export class UserValidator{
 
     }
 
+    async updateProfilePictureValidation(req, res, next){
+        try{
+            const { url } = req.body;
+            const userId = req.userId
+            const errors = []
+
+            if(!userId){
+                errors.push(`The userId cannot be blank.`)
+            }
+
+            if(!url){
+                errors.push(`The field url cannot be blank.`)
+            }
+
+            if(errors.length){
+                return res.status(400).json({errors});
+            }
+
+            next()
+
+        }catch(error){
+            console.log(error.message)
+
+            return res.status(500).json({
+                message: `${ERROS.INTERNAL} while validating request.`
+            })
+        }
+
+    }
+
+    async updateDescriptionValidation(req, res, next){
+        try{
+            const { description } = req.body;
+            const userId = req.userId
+            const errors = []
+
+            if(!userId){
+                errors.push(`The userId cannot be blank.`)
+            }
+
+            if(!description){
+                errors.push(`The field description cannot be blank.`)
+            }
+
+            if(errors.length){
+                return res.status(400).json({errors});
+            }
+
+            next()
+
+        }catch(error){
+            console.log(error.message)
+
+            return res.status(500).json({
+                message: `${ERROS.INTERNAL} while validating request.`
+            })
+        }
+
+    }
+
+    async updateCVValidation(req, res, next){
+        try{
+            const { url } = req.body;
+            const userId = req.userId
+            const errors = []
+
+            if(!userId){
+                errors.push(`The userId cannot be blank.`)
+            }
+
+            if(!url){
+                errors.push(`The field url cannot be blank.`)
+            }
+
+            if(errors.length){
+                return res.status(400).json({errors});
+            }
+
+            next()
+
+        }catch(error){
+            console.log(error.message)
+
+            return res.status(500).json({
+                message: `${ERROS.INTERNAL} while validating request.`
+            })
+        }
+
+    }
+
     async deleteUserValidation(req, res, next){
         try{
             const {password} = req.body
