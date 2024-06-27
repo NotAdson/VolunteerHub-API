4c0b6726cabc8dbd92fd88691fd41bbf04690467
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-06-27 14:55:30 -0300

    perf(Post.controller): adding error handling
    Adding proper error handling to all the functions.

--- server/src/controllers/Post.controller.js
+++ server/src/controllers/Post.controller.js
@@ -1,12 +1,19 @@
 import { PostService } from "../services/Post.service.js"
+import { ERROS } from "../shared/messages.js"
 
 const instancePostService = new PostService()
 
 export async function createPost(req, res){
-    const {title, content} = req.body
-    const userId = req.userId
-    const { statusValue, message} = await instancePostService.createPost(title, content, userId)
-    return res.status(statusValue).json({message: message})
+    try{
+        const {title, content} = req.body
+        const userId = req.userId
+        const { statusValue, message} = await instancePostService.createPost(title, content, userId)
+        return res.status(statusValue).json({message: message})
+    }catch(error){
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while creating post`})
+    }
+
 }
 
 export async function getPost(req, res){
@@ -15,7 +22,19 @@ export async function getPost(req, res){
         const response = await instancePostService.getPost(id)
         return res.status(response.statusValue).json({response})
     }catch(error){
-        return res.status(404).json({error: error.message})
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while getting post`})
+    }
+}
+
+export async function getAllPostsFromUser(req, res){
+    try{
+        const {userId} = req.params
+        const response = await instancePostService.getAllPostsFromUser(userId)
+        return res.status(response.statusValue).json({response})
+    }catch(error){
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while getting all posts from a specific user`})
     }
 }
 
@@ -26,9 +45,9 @@ export async function getAllPosts(req, res){
             response
         })        
     }catch(error){
-        return res.status(404).json({error: error.message})
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while getting all posts`})
     }
-
 }
 
 export async function updateTitle(req, res){
@@ -38,7 +57,8 @@ export async function updateTitle(req, res){
         const response = await instancePostService.updateTitle(userId, postId, newTitle)
         return res.status(response.statusValue).json({response})
     }catch(error){
-        return res.status(404).json({error: error.message})
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while updating title`})
     }
 }
 
@@ -49,7 +69,8 @@ export async function updateContent(req, res){
         const response = await instancePostService.updateContent(userId, postId, newContent)
         return res.status(response.statusValue).json({response})
     }catch(error){
-        return res.status(404).json({error: error.message})
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while updating content`})
     }
 }
 
@@ -61,6 +82,7 @@ export async function deletePost(req, res){
         
         return res.status(response.statusValue).json({response})
     }catch(error){
-        return res.status(404).json({error: error.message})
+        console.log(error.message)
+        return res.status(500).json({message: `${ERROS.INTERNAL} while deleting post`})
     }
 }
